4d4be59ea32d9da407711e61003d57b3801f8bf8
Ahmet Soner <ahmetsoner@morqet.com>
2022-02-14 18:36:27 +0300

    Message Types Created

---
+++ SocketSample.Common/MessageTypes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketSample.Common
+{
+    public enum MessageTypes : byte
+    {
+        Welcome = 100,
+        Bye = 110,
+        GoodBye = 111,
+
+        ClientList = 10,
+        NewClient = 11,
+        LeftClient = 12,
+
+        RoomList = 21,
+
+        MessageToAll = 30,
+        MessageToClient = 31,
+        MessageToRoom = 32,
+
+        SetUsername = 40,
+        ConfirmChangedUsername = 41,
+        NotifyChangedUsernameOthers = 42
+    }
+}
