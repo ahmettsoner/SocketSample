bf70cfe0bfd7f74bd70394dd86010224df0bbd51
Ahmet Soner <ahmettsoner@gmail.com>
2022-02-14 20:54:56 +0300

    Commont DataPackage, Event and MessageInfo classes created and delegates defined

---
+++ SocketSample.Common/DataPackage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class DataPackage
+    {
+        public string Data { get; set; }
+        public MessageTypes Type { get; set; }
+
+        public DataPackage(MessageTypes type, string data)
+        {
+            this.Type = type;
+            this.Data = data;
+        }
+
+        public DataPackage(byte[] data, int limit)
+        {
+            if (data.Length > 0)
+            {
+                this.Type = (MessageTypes)data[0];
+                this.Data = Encoding.ASCII.GetString(data, 1, limit - 1);
+            }
+        }
+
+        public byte[] ToByteArray()
+        {
+            List<byte> result = new List<byte>();
+
+            result.Add((byte)this.Type);
+
+            if (!string.IsNullOrEmpty(this.Data))
+            {
+                var data = Encoding.ASCII.GetBytes(this.Data);
+                result.AddRange(data);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ SocketSample.Common/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class Event : BaseObject
+    {
+        private DateTime _Timestamp;
+        public DateTime Timestamp { get { return _Timestamp; } set { _Timestamp = value; OnPropertyChanged(nameof(Timestamp)); } }
+        private string _Content;
+        public string Content { get { return _Content; } set { _Content = value; OnPropertyChanged(nameof(Content)); } }
+
+        public Event(string Content)
+        {
+            this.Timestamp = DateTime.Now;
+            this.Content = Content;
+        }
+        public Event(DateTime Timestamp, string Content)
+        {
+            this.Timestamp = Timestamp;
+            this.Content = Content;
+        }
+    }
+}
